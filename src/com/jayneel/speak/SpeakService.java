6731132157e824455140b665e83171c2d11c4733
Sandip Chitale <Sandip.Chitale@emc.com>
2014-12-22 11:28:51 -0800

    Initial commit.

---
+++ src/com/jayneel/speak/SpeakService.java
@@ -0,0 +1,121 @@
+package com.jayneel.speak;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.concurrent.CountDownLatch;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class SpeakService extends IntentService {
+	private TextToSpeech textToSpeech;
+	private CountDownLatch countDownLatch;
+	static boolean stopIt = false;
+
+	/**
+	 * Creates an IntentService. Invoked by your subclass's constructor.
+	 *
+	 */
+	public SpeakService() {
+		super("Speak");
+		countDownLatch = new CountDownLatch(1);
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		textToSpeech = new TextToSpeech(getApplicationContext(),
+				new TextToSpeech.OnInitListener() {
+					@Override
+					public void onInit(int status) {
+						if (status != TextToSpeech.ERROR) {
+							textToSpeech.setLanguage(Locale.US);
+							countDownLatch.countDown();
+							Log.d(Thread.currentThread().getName(), "TTS Initialized.");
+						}
+					}
+				});
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		String action = intent.getAction();
+		Log.d(Thread.currentThread().getName(), "Action: " + action);
+		if ("SPEAK".equals(action)) {
+			String text = intent.getExtras().getString("text");
+			if (text != null) {
+				try {
+					countDownLatch.await();
+					Thread.sleep(100);
+					say(text);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private void say(String text) {
+		textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, new HashMap<String, String>());
+		NotificationManager mNotificationManager =
+				(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        
+		Intent stopIntent = new Intent(this, SpeakServiceLaunchActivity.class);
+		stopIntent.setAction("STOP");
+		
+		PendingIntent stopPendingIntent =
+				PendingIntent.getActivity(
+						this,
+						10001,
+						stopIntent,
+						PendingIntent.FLAG_CANCEL_CURRENT);
+		        				
+		NotificationCompat.Builder notficationBuilder = new NotificationCompat.Builder(this)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Speak")
+				.setContentText("Touch to stop.")
+				.setPriority(Notification.PRIORITY_MAX);
+		
+		notficationBuilder.setContentIntent(stopPendingIntent);
+		notficationBuilder.addAction(R.drawable.ic_av_stop, "STOP", stopPendingIntent);
+		
+		mNotificationManager.notify(1001, notficationBuilder.build());
+		stopIt = false;
+		waitToFinishSpeaking();
+	}
+	
+	private void waitToFinishSpeaking() {
+		try {
+			while (textToSpeech.isSpeaking()) {
+				if (stopIt) {
+					stopIt = false;
+					textToSpeech.stop();
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+			}
+		} finally {
+			NotificationManager mNotificationManager =
+					(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			mNotificationManager.cancel(1001);
+		}
+	}
+
+
+	@Override
+	public void onDestroy() {
+		textToSpeech.shutdown();
+		super.onDestroy();
+	}
+
+}
---
+++ src/com/jayneel/speak/SpeakServiceLaunchActivity.java
@@ -0,0 +1,74 @@
+package com.jayneel.speak;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class SpeakServiceLaunchActivity extends Activity {
+	private boolean finishAfterIntent = true;
+	private EditText speechText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_speak_service_launch);
+		speechText = (EditText) findViewById(R.id.speechText);
+		Intent intent = getIntent();
+        String action = intent.getAction();
+		Log.d(Thread.currentThread().getName(), "Action: " + action);
+        if (Intent.ACTION_MAIN.equals(action)) {
+        	finishAfterIntent = false;
+
+        } else if (Intent.ACTION_SEND.equals(action)) {
+        	String type = intent.getType();
+            if ("text/plain".equals(type)) {
+            	String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+            	speak(sharedText);
+            }
+        } else if ("STOP".equals(action)) {
+        	stop();
+        }
+        
+    	if (finishAfterIntent) {
+    		finish();
+    	}
+	}
+	
+	private void speak(String text) {
+		Intent speakIntent = new Intent(this, SpeakService.class);
+    	speakIntent.setAction("SPEAK");
+    	speakIntent.putExtra("text", text);
+    	this.startService(speakIntent);
+	}
+	
+	private void stop() {
+		SpeakService.stopIt = true;
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.speak_service_launch_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_speak) {
+			speak(speechText.getText().toString());
+		} else if (id == R.id.action_stop) {
+			stop();
+		} else if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
